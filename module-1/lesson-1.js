ba721f404610c83a19b739ed13eb678555177cde
grypis <grypisnecros@gmail.com>
2024-10-03 12:03:55 +0300

    start

---
+++ module-1/lesson-1.js
@@ -0,0 +1,61 @@
+'use strict';
+// console.log("Hello world!");
+// console.log("Value");
+// console.log("JS is Awesome");
+// console.log(10);
+
+// const age = 20;
+// console.log(age);
+
+// const username = "Mango";
+// console.log(username);
+
+// let username = "Mango";
+// console.log(username);
+
+// username = "Oliver";
+// console.log(username);
+
+
+// const age = 20;
+// console.log(age);
+// const salary = 241.1;
+// console.log(salary);
+
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+
+// const quantity = 17;
+// console.log(typeof quantity);
+// const message = "JavaScript is awesome!";
+// console.log(typeof message);
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen);
+// let username;
+// console.log(typeof username);
+// let status = null;
+// console.log(typeof status);
+
+
+
+
+
+
+
+// Tasks from js redactor
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+let productName = "Droid";
+let pricePerItem = 2000;
+
+productName = "Repair droid";
+console.log(productName);
+
+pricePerItem = 3500;
+console.log(pricePerItem);
