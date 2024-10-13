bc00292e3c888f9b15ffb953e98ae9f5898c3532
grypis <grypisnecros@gmail.com>
2024-10-13 19:06:59 +0300

    ss

--- module-1/lesson-1.js
+++ module-1/lesson-1.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // console.log("Hello world!");
 // console.log("Value");
 // console.log("JS is Awesome");
@@ -16,19 +16,16 @@
 // username = "Oliver";
 // console.log(username);
 
-
 // const age = 20;
 // console.log(age);
 // const salary = 241.1;
 // console.log(salary);
-
 
 // const topSpeed = 160;
 // const distance = 617.54;
 // const login = "mango935";
 // const isOnline = true;
 // const isAdmin = false;
-
 
 // const quantity = 17;
 // console.log(typeof quantity);
@@ -41,21 +38,80 @@
 // let status = null;
 // console.log(typeof status);
 
+// const x = 5;
+// const y = 10;
+// console.log(x + y);
 
+// const a = 7;
+// const b = 5;
+// console.log(a - b);
 
+// const c = 4;
+// const d = 5;
+// console.log(c + d);
+// console.log(c - d);
+// console.log(c * d);
+// console.log(c / d);
+// console.log(c % d);
+// console.log(c ** d);
 
+// let age = 25;
+// age = age + 1;
+// console.log(age);
 
+// let age = 25;
+// age += 1;
+// console.log(age);
 
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// Change code below this line
+// a += 2;
+// console.log(a);
+// b -= 4;
+// console.log(b);
+// c *= 3;
+// console.log(c);
+// d /= 10;
+// console.log(d);
 
 // Tasks from js redactor
 // const productName = "Droid";
 // const pricePerItem = 2000;
 
-let productName = "Droid";
-let pricePerItem = 2000;
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName = "Repair droid";
+// console.log(productName);
+// pricePerItem = 3500;
+// console.log(pricePerItem);
 
-productName = "Repair droid";
-console.log(productName);
+const pricePerItem = 3500;
+const orderedQuantity = 4;
 
-pricePerItem = 3500;
-console.log(pricePerItem);
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice);
+
+// ===========================================
+// Рядки
+
+// const message = "Mango " + "is" + " happy";
+// console.log(message);
+
+// const age = 25;
+// const message = "Mango is " + age + " years old";
+// console.log(message);
+
+// console.log("Mango" + 55);
+// console.log("Mango" + true);
+
+// console.log(1 + "2");
+// console.log(1 + "2" + 4);
+// console.log(1 + 2 + "4");
+
+// const username = "Poly";
+// // const message = "Welcome " + username + "!";
+// const message = `Welcome ${username}!`;
+// console.log(message);
