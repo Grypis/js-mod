b6ab954a06dff115367c873aa11d865f7e39dd22
grypis <grypisnecros@gmail.com>
2024-10-24 01:37:43 +0300

    sa

---
+++ module-1/lesson-1.1.js
@@ -0,0 +1,62 @@
+// 01 - vars;
+// console.log(10);
+// console.log(20 + 10 + 20);
+// console.log('Hello world!');
+// console.log('40');
+// console.log(40);
+
+// let age = 10;
+// console.log(age);
+
+// let x = 10;
+// let y = 50;
+// console.log(x + y);
+
+// let age;
+// age = 10;
+// age = 20;
+// console.log(age);
+
+// const age = 20;
+// console.log(age);
+
+// const x = 10;
+// const y = x + 10;
+// console.log(y);
+
+//! info
+// const x = NaN;
+// const result = typeof x;
+// console.log(result);
+
+//! 02-operators
+/* const a = 10;
+const b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+//* Підведення до ступіню = 8
+console.log(2 ** 3);
+//* Скільки разів 3 поміщається в 10 = 3, = остача
+// 10 % 3 = 10 - (3 * 3) = 1
+// 11 % 3 = 10 - (3 * 3) = 2
+// 12 % 3 = 10 - (3 * 4) = 0
+console.log(10 % 3); */
+
+/* const apples = 47;
+const grapes = 135;
+
+const result1 = apples + grapes;
+const result2 = apples - grapes;
+
+console.log('Result 1', result1);
+console.log('Result 2', result2);
+ */
+
+/* let students = 100;
+//! students = students + 50;
+students += 50;
+students -= 50;
+console.log(students); */
---
+++ module-3/lesson-1.js
@@ -0,0 +1,57 @@
+/* const fruits = ["apple", "plum", "pear", "orange"];
+console.log(fruits); */
+
+//! arrayName[index]
+/* const planets = ["Earth", "Mars", "Venus"];
+console.log(planets[0]); // 'Earth'
+console.log(planets[1]); // 'Mars'
+console.log(planets[2]); // 'Venus'
+ */
+
+/* const planets = ["Earth", "Mars", "Venus"];
+const firstElement = planets[0];
+console.log(firstElement); // 'Earth'
+console.log(planets[3]); // undefined */
+
+/* const fruits = ["apple", "plum", "pear", "orange"];
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];
+console.log(firstElement, secondElement, lastElement);
+ */
+
+/* const planets = ["Earth", "Mars", "Venus", "Uranus"];
+planets[0] = "Jupiter";
+planets[2] = "Neptune";
+console.log(planets);
+ */
+
+/* 
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+fruits[1] = 'peach';
+fruits[3] = 'banana';
+console.log(fruits); */
+
+/* const planets = ['Earth', 'Mars', 'Venus'];
+console.log(planets.length); // 3 */
+
+/* const planets = ['Earth', 'Mars', 'Venus'];
+if (planets.length >= 3) {
+  console.log('3 or more elements');
+} else {
+  console.log('3 or less elements');
+}
+
+const refr = planets.length >= 3 ? '3 or more elements' : '3 or less elements';
+console.log(refr);
+ */
+
+/* function getOrderQuantity(order) {
+  return order.length;
+}
+console.log(getOrderQuantity(['apple', 'peach', 'pear', 'banana'])); // 4
+console.log(getOrderQuantity(['apple', 'banana'])); // 2 */
+
+/* const planets = ['Earth', 'Mars', 'Venus'];
+const lastElementIndex = planets.length - 1;
+console.log(planets[lastElementIndex]); // 'Venus' */
