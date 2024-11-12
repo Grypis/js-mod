76946984be95d6ddf258856072841359433e7e34
grypis <grypisnecros@gmail.com>
2024-11-12 18:35:38 +0200

    adad

--- module-5/lesson-1.js
+++ module-5/lesson-1.js
@@ -92,3 +92,83 @@ numbers.forEach(function (number, index) {
 console.log(calculateTotalPrice([12, 85, 37, 4])); */
 
 // function filterArray(numbers, value) {}
+
+/* function classicAdd(a, b, c) {
+  return a + b + c;
+}
+
+const arrowAdd = (a, b, c) => {
+  return a + b + c;
+} */
+
+/* const add = (a, b, c) => {
+  return a + b + c;
+} */
+
+/* const add = a => {
+  return a + 5;
+} */
+
+/* const greet = () => {
+  console.log('Hello!');
+};
+greet(); */
+
+/* function calculateTotalPrice(quantity, pricePerItem) {
+  return quantity * pricePerItem;
+} */
+
+/* const calculateTotalPrice = (quantity, pricePerItem) => {
+  return quantity * pricePerItem;
+};
+console.log(calculateTotalPrice(5, 100));
+ */
+
+/* const add = (a, b, c) => {
+  console.log(a, b, c);
+  return a + b + c;
+};
+ */
+
+/* const add = (a, b, c) => a + b + c;
+console.log(add(2, 3, 454)); */
+
+/* function classicADd(a, b, c) {
+  return a + b + c;
+}
+
+const add = (a, b, c) => a + b + c; */
+
+/* const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+console.log(calculateTotalPrice(2, 3)); */
+
+/* const add = (...args) => {
+  console.log(args);
+};
+add(1, 2, 3); */
+
+/* const numbers = [5, 10, 15, 20, 25];
+numbers.forEach(function (number, index) {
+  console.log(`Index ${index}, value ${number}`);
+});
+numbers.forEach((number, index) => {
+  console.log(`Index ${index}, value${number}`);
+}); */
+
+/* const numbers = [5, 10, 15, 20, 25];
+const logMessage = (numbers, index) => {
+  console.log(`Index ${index}, value ${numbers}`);
+};
+numbers.forEach(logMessage); */
+
+/* const calculateTotalPrice = orderedItems => {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+};
+console.log(calculateTotalPrice([12, 85, 37, 4])); */
