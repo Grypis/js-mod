6ba85e74e9e42256740d5f451c41b141217ed149
grypis <grypisnecros@gmail.com>
2024-12-14 15:41:15 +0200

    sol

---
+++ module-7/lesson-1.js
@@ -0,0 +1,12 @@
+//? element.querySelector(selector);
+/* const item = document.querySelector('.list-item');
+console.log(item); */
+
+//? element.querySelectorsAll(selector)
+/* const listItems = document.querySelectorAll('.list-item');
+console.log(listItems); */
+
+/* const link = document.querySelector('.link');
+console.log(link.href); // https://goit.global/
+link.href = 'https://neo.goit.global/';
+console.log(link.href); */
