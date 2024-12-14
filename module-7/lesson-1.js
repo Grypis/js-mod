0353d29b1a62c085f80e8edfc56262e29f31b6c6
grypis <grypisnecros@gmail.com>
2024-12-14 16:38:13 +0200

    ls

--- module-7/lesson-1.js
+++ module-7/lesson-1.js
@@ -10,3 +10,34 @@ console.log(listItems); */
 console.log(link.href); // https://goit.global/
 link.href = 'https://neo.goit.global/';
 console.log(link.href); */
+
+/* const el = document.querySelector('.text');
+const nested = document.querySelector('.sub-text');
+console.log(el.textContent);
+console.log(nested.textContent);
+el.textContent = 'Username: Poly';
+console.log(el.textContent); */
+
+const link = document.querySelector('.link');
+console.log(link.classList);
+
+// const hasActiveClass = link.classList.contains('is-active'); // true
+// const hasActiveClass = link.classList.contains('title'); // false
+// console.log(hasActiveClass);
+
+/* const link = document.querySelector('a');
+const hasActiveClass = link.classList.contains('title'); // false
+console.log(hasActiveClass); */
+
+/* link.classList.add('special');
+console.log(link.classList); */
+
+/* link.classList.remove('is-active');
+console.log(link.classList);
+ */
+
+/* link.classList.toggle('is-active');
+console.log(link.classList); */
+
+/* link.classList.replace('special', 'regular');
+console.log(link.classList); */
