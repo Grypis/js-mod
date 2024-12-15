0c003b0fbb689a2d0e7ba85d7eb074bc0c9eb840
grypis <grypisnecros@gmail.com>
2024-12-15 08:33:45 +0200

    asddas

--- module-7/lesson-1.js
+++ module-7/lesson-1.js
@@ -18,8 +18,8 @@ console.log(nested.textContent);
 el.textContent = 'Username: Poly';
 console.log(el.textContent); */
 
-const link = document.querySelector('.link');
-console.log(link.classList);
+/* const link = document.querySelector('.link');
+console.log(link.classList); */
 
 // const hasActiveClass = link.classList.contains('is-active'); // true
 // const hasActiveClass = link.classList.contains('title'); // false
@@ -41,3 +41,63 @@ console.log(link.classList); */
 
 /* link.classList.replace('special', 'regular');
 console.log(link.classList); */
+
+/* const button = document.querySelector('.btn');
+button.style.backgroundColor = 'teal';
+button.style.fontSize = '24px';
+button.style.textAlign = 'center';
+console.log(button.style);
+ */
+
+// const image = document.querySelector('.image');
+/* console.log(image.hasAttribute('src'));
+console.log(image.hasAttribute('href')); */
+// console.log(image.getAttribute('alt'));
+/* image.setAttribute('alt', 'Amazing nature');
+console.log(image.getAttribute('alt')); */
+/* image.removeAttribute('alt');
+console.log(image.hasAttribute('alt')); */
+
+/* const saveBtn = document.querySelector('button[data-action="save"]');
+console.log(saveBtn.dataset.action);
+
+const closeBtn = document.querySelector('button[data-action="close"]');
+console.log(closeBtn.dataset.action);
+
+saveBtn.dataset.action = 'update';
+saveBtn.dataset.role = 'admin';
+console.log(saveBtn.dataset.action);
+console.log(saveBtn.dataset.role); */
+
+//? document.createElement(tagName)
+/* const heading = document.createElement('h1');
+heading.classList.add('title');
+heading.textContent = 'This is a heading';
+console.log(heading);
+heading.prepend(firstItem); */
+
+/* const image = document.createElement('img');
+image.src = 'https://picsum.photos/id/11/320/240';
+image.alt = 'Nature';
+console.log(image); */
+
+/* const heading = document.createElement('h1');
+heading.classList.add('title');
+heading.textContent = 'This is a heading';
+
+// Створюємо дочірній елемент
+const firstItem = document.createElement('span');
+firstItem.textContent = 'This is the first item';
+
+// Додаємо дочірній елемент до heading
+heading.prepend(firstItem);
+
+// Додаємо heading до body
+document.body.prepend(heading);
+
+console.log(heading); */
+
+/* const text = document.querySelector('text');
+text.remove(); */
+
+//? element.insertAdjacentHTML(position, string)
