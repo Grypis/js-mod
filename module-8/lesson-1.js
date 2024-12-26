102e04dfc0b05bffce78dd855924ad8913be7fc6
grypis <grypisnecros@gmail.com>
2024-12-26 17:18:34 +0200

    sos

--- module-8/lesson-1.js
+++ module-8/lesson-1.js
@@ -64,3 +64,142 @@ console.log(_.shuffle([1, 2, 3, 4]));
     closable: false,
   },
 ); */
+
+//! ====================================================
+/* const user = {
+  name: 'Jacob',
+  age: 32,
+};
+
+console.log(user.name);
+console.log(user.age);
+
+const { name, age } = user;
+console.log(name);
+console.log(age); */
+
+//! ====================================================
+/* const book = {
+  title: 'The last Kingdom',
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  isPublic: true,
+  rating: 8.38,
+};
+
+const { title, author, genres, inPublic, rating } = book;
+
+const accessType = book.isPublic ? 'public' : 'private';
+// const message = `Book ${book.title} by author ${book.author} with rating ${book.rating} is in ${accessType} access!`;
+
+const message = `Book ${title} by author ${author} with rating ${rating} is in ${accessType} access!`;
+console.log(message); */
+
+//! ====================================================
+/* const book = {
+  title: 'The Last Kingdom',
+  author: 'Bernard Cornwell',
+};
+
+const {
+  title,
+  author,
+  coverImage = 'https://via.placeholder.com/640/480',
+} = book;
+console.log(title);
+console.log(coverImage); */
+
+//! ====================================================
+/* const book = {
+  title: 'The Last Kingdom',
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  isPublic: true,
+  rating: 8.38,
+};
+
+const { title, author: bookAuthor, isPublic, rating: bookRating } = book;
+console.log(title);
+console.log(bookAuthor);
+console.log(isPublic);
+console.log(bookRating); */
+
+//! ====================================================
+/* const book = {
+  title: 'The Last Kingdom',
+  coverImage:
+    'https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg',
+};
+
+const {
+  title,
+  coverImage: bookCoverImage = 'https://via.placeholder.com/640/480',
+} = book;
+console.log(title);
+console.log(bookCoverImage); */
+
+//! ====================================================
+/* const book = {
+  title: 'The Dream of a Ridiculous Man',
+};
+
+const {
+  title,
+  coverImage: bookCoverImage = 'https://via.placeholder.com/640/480',
+} = book;
+console.log(title);
+console.log(bookCoverImage); */
+
+//! ====================================================
+/* const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+];
+
+for (const book of books) {
+  console.log(book.title);
+  console.log(book.author);
+  console.log(`до деструктиризації = ${book.rating}`);
+}
+
+for (const book of books) {
+  const { title, author, rating } = book;
+
+  console.log(title);
+  console.log(author);
+  console.log(rating);
+}
+
+for (const { title, author, rating } of books) {
+  console.log(title);
+  console.log(author);
+  console.log(rating);
+} */
+
+//! ====================================================
+/* function printUserInfo(user) {
+  console.log(`Name: ${user.name}, Age: ${user.age}, Hobby: ${user.hobby}`);
+} */
+
+/* printUserInfo({
+  name: 'Alice',
+  age: 25,
+  hobby: 'dancing',
+}); */
+
+/* function printUserInfo(user) {
+  const { name, age, hobby } = user;
+  console.log(`Name: ${name}, Age: ${age}, Hobby: ${hobby}`);
+} */
+
+/* function printUserInfo({ name, age, hobby }) {
+  console.log(`Name: ${name}, Age: ${age}, Hobby: ${hobby}`);
+} */
