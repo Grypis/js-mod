fb1e989ea2423d4f32977a8964f542f39b2b014f
grypis <grypisnecros@gmail.com>
2025-01-26 00:29:49 +0200

    asd

--- module-10/lesson-1.js
+++ module-10/lesson-1.js
@@ -194,3 +194,134 @@ fetchUserFromServer('Mango')
   .catch(error => console.error(error)); */
 
 //! ====================================
+/* new Promise(resolve => resolve('success')).then(value =>
+  console.log(value).catch(error => console.log(error)),
+); */
+
+/* Promise.resolve('success').then(value =>
+  console.log(value).catch(error => console.log(error)),
+); */
+
+/* new Promise((resolve, reject) => reject('error'))
+  .then(value => console.log(value))
+  .catch(error => console.log(error)); */
+
+/* Promise.reject('error')
+  .then(value => console.log(value))
+  .catch(error => console.log(error)); */
+
+//! ====================================
+/* const makeGreeting = (guestName, onSuccess, onError) => {
+  if (!guestName) {
+    onError('Guest name must not be empty');
+  } else {
+    onSuccess(`Welcome ${guestName}`);
+  }
+};
+
+makeGreeting(
+  'Mango',
+  greeting => console.log(greeting),
+  error => console.error(error),
+); */
+
+/* const makeGreeting = guestName => {
+  return new Promise((resolve, reject) => {
+    if (!guestName) {
+      reject('Guest name must not be empty');
+    } else {
+      resolve(`Welcome ${guestName}`);
+    }
+  });
+};
+
+makeGreeting('Mango')
+  .then(greeting => console.log(greeting))
+  .catch(error => console.error(error)); */
+
+/* const makeGreeting = guestName => {
+  if (!guestName) {
+    return Promise.reject('Guest name must not be empty');
+  } else {
+    return Promise.resolve(`Welcome ${guestName}`);
+  }
+};
+
+makeGreeting('Mango')
+  .then(greeting => console.log(greeting))
+  .catch(error => console.error(error)); */
+
+//! ====================================
+/* const makePromise = ({ value, delay, shouldResolve = true }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(value);
+      } else {
+        reject(value);
+      }
+    }, delay);
+  });
+};
+
+// makePromise({
+//   value: 'Some value',
+//   delay: 2000,
+//   shouldResolve: true,
+// });
+
+makePromise({ value: 'A', delay: 1000 }).then(value =>
+  console.log(value).catch(error => console.log(error)),
+);
+ */
+
+/* new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Fulfilled A');
+  }, 1000);
+})
+  .then(value => console.log(value))
+  .catch(error => console.log(error)); */
+
+//! ====================================
+//* Promise.all([promise1, promise2, promise3, ...])
+
+/* const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+const p3 = Promise.resolve(3);
+
+Promise.all([p1, p2, p3])
+  .then(values => console.log(values))
+  .catch(error => console.log(error)); */
+
+/* const p1 = Promise.resolve(1);
+const p2 = Promise.reject('Rejected promise 2');
+const p3 = Promise.resolve(3);
+
+Promise.all([p1, p2, p3])
+  .then(values => console.log(values))
+  .catch(error => console.log(error)); */
+
+//! ====================================
+//* Promise.allSetteled([promise1, promise2, promise3, ...])
+
+/* const p1 = Promise.resolve(1);
+const p2 = Promise.reject('Rejected promise 2');
+const p3 = Promise.resolve(3);
+
+Promise.allSettled([p1, p2, p3]).then(values => console.log(values)); */
+
+//! ====================================
+//* Promise.race([promise1, promise2, promise3, ...])
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(2), 2000);
+});
+
+Promise.race([p1, p2])
+  .then(value => console.log(value))
+  .catch(error => console.log(error));
